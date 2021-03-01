534b720b1968a06ad6a97bef084a06fb2f5f1e42
samuel schwob <samuelschwob@gmail.com>
2021-03-01 16:06:07 +0100

    Setup cloudinary

--- app/models/island.rb
+++ app/models/island.rb
@@ -1,6 +1,7 @@
 class Island < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
   validates :country, :description, :latitude, :longitude, :price_per_day, :area, presence: true
