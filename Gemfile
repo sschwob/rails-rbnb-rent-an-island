b0d5c637c57a889a425833a97d278a9edbb77c32
kevin sayarath <kevin.sayarath@hotmail.fr>
2021-03-01 17:10:58 +0100

    Methods Create and New are added on IslandsController

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'devise'
 
+gem 'country_select'
+
 group :development, :test do
   gem 'pry-byebug'
   gem 'pry-rails'
--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -1,2 +1,17 @@
 class IslandsController < ApplicationController
+
+  def new
+    @island = Island.new
+  end
+
+  def create
+    @island = Island.new(island_params)
+    @island.save
+
+    redirect_to island_path(@island)
+  end
+
+  def island_params
+    params.require(:island).permit(:name, :country, :description, :latitude, :longitude, :price_per_day, :area, :photo)
+  end
 end
