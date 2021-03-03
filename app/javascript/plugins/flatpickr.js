912d205f7a976d05358ce6c216fe479665bdb6e9
samuel schwob <samuelschwob@gmail.com>
2021-03-03 22:30:23 +0100

    Dynamic calculation of the price of a booking

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,7 +4,17 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 const initFlatpickr = () => {
   flatpickr("#range_start", {
     altInput: true,
-    plugins: [new rangePlugin({ input: "#range_end"})]
+    plugins: [new rangePlugin({ input: "#range_end"})],
+    minDate: "today",
+    dateFormat: 'n/j/Y',
+    onClose: (selectedDates, dateStr, instance) => {
+      const daysInRange = document.getElementsByClassName('inRange');
+      const daysLengthTotal = daysInRange.length + 1;
+      const daysField = document.getElementById('length-of-stay');
+      const totalPriceField = document.getElementById('total-price')
+      daysField.innerText = daysLengthTotal;
+      totalPriceField.innerText = daysLengthTotal * totalPriceField.dataset.price;
+    }
   });
 }
 
