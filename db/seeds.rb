e3966c764fbd016199be182f93fe9a3da5440865
maxime ayllon <maximeayllon12@gmail.com>
2021-03-05 11:59:44 +0100

    search bar index for phone size

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,13 @@ Island.destroy_all
 
 puts "Create islands"
 Island.create!(name: "Ambergris Caye", country: "BZ", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                latitude: 18.0050, longitude: 87.5552, price_per_day: 15000, area: 64, user_id: 1 )
+                latitude: 18.001592, longitude: -87.94328370000001, price_per_day: 15000, area: 64, user_id: 1 )
 
 Island.create!(name: "Bora Bora", country: "FR", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  latitude: 16.2940, longitude: 151.4411, price_per_day: 17500, area: 38, user_id: 1 )
+  latitude: -16.5004126, longitude: -151.7414904, price_per_day: 17500, area: 38, user_id: 1 )
 
 Island.create!(name: "Santorin", country: "GR", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  latitude: 36.2317, longitude: 25.2735, price_per_day: 25500, area: 76, user_id: 1 )
+  latitude: 36.3931562, longitude: 25.4615092, price_per_day: 25500, area: 76, user_id: 1 )
 
 Island.create!(name: "Nosy be", country: "MG", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   latitude: 13.2000, longitude: 48.15, price_per_day: 57400, area: 321, user_id: 1 )
